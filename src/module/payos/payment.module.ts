2fcc58ee718ccf9928bef823af4409690632a49a
tTien <Huynhthanhtien830@gmail.com>
2025-05-20 18:27:09 +0700

    update main.ts

--- src/module/payos/payment.module.ts
+++ src/module/payos/payment.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { BillService } from '../bill/bill.service';
+import { BillModule } from '../bill/bill.module';
 
 @Module({
-  providers: [PaymentService, BillService],
+  providers: [PaymentService],
   controllers: [PaymentController],
   exports: [PaymentService],
-  // imports: [BillService],
+  imports: [BillModule],
 })
 export class PaymentModule {}
